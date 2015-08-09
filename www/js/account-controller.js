ff69c959c4b98b6be3d92c5eefe1345c5ceb1cd3
Renato Ramiro <renato.ipj@gmail.com>
2015-08-09 20:11:26 -0300

    Clear user data

--- www/js/account-controller.js
+++ www/js/account-controller.js
@@ -2,16 +2,17 @@ angular.module('starter.account.controller', [])
 
 .controller('AccountCtrl', function ($scope, $state, $ionicModal) {
   $scope.modal = {};
+  $scope.user = {};
   $scope.list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   $scope.recording = false;
 
   $scope.signIn = function (user) {
-    console.log(user);
+    $scope.user = {};
     $state.go('master.home');
   };
 
   $scope.signUp = function (user) {
-    console.log(user);
+    $scope.user = {};
     $state.go('master.home');
   };
 
